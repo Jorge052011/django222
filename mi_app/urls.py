218c2d209e5fe75c4228ee3f4ffd5e8384e38547
Jorge052011 <jimunozacuna+1@gmail.com>
2025-07-26 00:45:15 +0000

    W

---
+++ mi_app/urls.py
@@ -0,0 +1,12 @@
+
+
+
+from .views import home,test
+
+from django.urls import path
+
+urlpatterns = [
+   
+    path("home/", home, name="home"),
+    path("test/<str:variable>", test, name="test"),
+]
